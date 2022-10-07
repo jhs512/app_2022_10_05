d4452354a17711f0497b953a58e9c9b4a9b174b9
jhs512 <jangka512@gmail.com>
2022-10-07 14:23:45 +0900

    테스트용 게시물 2건 추가

---
+++ src/main/java/com/ll/exam/app__2022_10_05/app/article/entity/Article.java
@@ -0,0 +1,30 @@
+package com.ll.exam.app__2022_10_05.app.member.entity;
+
+import com.fasterxml.jackson.annotation.JsonIgnore;
+import com.ll.exam.app__2022_10_05.app.base.entity.BaseEntity;
+import lombok.*;
+import lombok.experimental.SuperBuilder;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.ManyToOne;
+
+@Entity
+@Setter
+@Getter
+@AllArgsConstructor
+@NoArgsConstructor
+@SuperBuilder
+@ToString(callSuper = true)
+public class Article extends BaseEntity {
+    @ManyToOne
+    private Member author;
+    @Column(unique = true)
+    private String subject;
+    @JsonIgnore
+    private String content;
+
+    public Article(long id) {
+        super(id);
+    }
+}
