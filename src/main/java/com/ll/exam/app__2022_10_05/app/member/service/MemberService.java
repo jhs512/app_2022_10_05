6543eada62a78fb5a0318092941ce11919fbe8c5
jhs512 <jangka512@gmail.com>
2022-10-13 13:12:17 +0900

    캐시 사용

--- src/main/java/com/ll/exam/app__2022_10_05/app/member/service/MemberService.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.ll.exam.app__2022_10_05.app.member.entity.Member;
 import com.ll.exam.app__2022_10_05.app.member.repository.MemberRepository;
 import com.ll.exam.app__2022_10_05.app.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
 import org.springframework.util.StringUtils;
@@ -36,7 +37,7 @@ public class MemberService {
     public String genAccessToken(Member member) {
         String accessToken = member.getAccessToken();
 
-        if (StringUtils.hasLength(accessToken) == false ) {
+        if (StringUtils.hasLength(accessToken) == false) {
             accessToken = jwtProvider.generateAccessToken(member.getAccessTokenClaims(), 60L * 60 * 24 * 365 * 100);
             member.setAccessToken(accessToken);
         }
@@ -46,5 +47,11 @@ public class MemberService {
 
     public boolean verifyWithWhiteList(Member member, String token) {
         return member.getAccessToken().equals(token);
+    }
+
+    @Cacheable("key1")
+    public int getCachedInt() {
+        System.out.println("getCachedInt 호출됨");
+        return 5;
     }
 }
