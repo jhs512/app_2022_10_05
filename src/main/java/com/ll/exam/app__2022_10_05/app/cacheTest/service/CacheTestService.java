124e8b409645fed5eba742a4ac3f9fb5519be808
jhs512 <jangka512@gmail.com>
2022-10-13 14:05:47 +0900

    매개변수 캐싱

---
+++ src/main/java/com/ll/exam/app__2022_10_05/app/cacheTest/service/CacheTestService.java
@@ -0,0 +1,31 @@
+package com.ll.exam.app__2022_10_05.app.cacheTest.service;
+
+import org.springframework.cache.annotation.CacheEvict;
+import org.springframework.cache.annotation.CachePut;
+import org.springframework.cache.annotation.Cacheable;
+import org.springframework.stereotype.Service;
+
+@Service
+public class CacheTestService {
+
+    @Cacheable("key1")
+    public int getCachedInt() {
+        System.out.println("getCachedInt 호출됨");
+        return 5;
+    }
+
+    @CacheEvict("key1")
+    public void deleteCacheKey1() {
+    }
+
+    @CachePut("key1")
+    public int putCacheKey1() {
+        return 10;
+    }
+
+    @Cacheable("plus")
+    public int plus(int a, int b) {
+        System.out.println("== plus 실행 ==");
+        return a + b;
+    }
+}
--- src/test/java/com/ll/exam/app__2022_10_05/CacheTests.java
+++ src/test/java/com/ll/exam/app__2022_10_05/CacheTests.java
@@ -1,6 +1,6 @@
 package com.ll.exam.app__2022_10_05;
 
-import com.ll.exam.app__2022_10_05.app.member.service.MemberService;
+import com.ll.exam.app__2022_10_05.app.cacheTest.service.CacheTestService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
 import org.springframework.beans.factory.annotation.Autowired;
@@ -8,53 +8,64 @@ import org.springframework.boot.test.context.SpringBootTest;
 import org.springframework.test.context.ActiveProfiles;
 import org.springframework.transaction.annotation.Transactional;
 
-import static org.assertj.core.api.Assertions.assertThat;
-
 
 @SpringBootTest
 @Transactional
 @ActiveProfiles("test")
 class CacheTests {
     @Autowired
-    private MemberService memberService;
+    private CacheTestService cacheTestService;
 
     @Test
     @DisplayName("캐시 사용")
     void t1() throws Exception {
-        int rs = memberService.getCachedInt();
+        int rs = cacheTestService.getCachedInt();
         System.out.println(rs);
 
-        rs = memberService.getCachedInt();
+        rs = cacheTestService.getCachedInt();
         System.out.println(rs);
     }
 
     @Test
     @DisplayName("캐시 삭제")
     void t2() throws Exception {
-        int rs = memberService.getCachedInt();
+        int rs = cacheTestService.getCachedInt();
         System.out.println(rs);
 
-        rs = memberService.getCachedInt();
+        rs = cacheTestService.getCachedInt();
         System.out.println(rs);
 
-        memberService.deleteCacheKey1();
+        cacheTestService.deleteCacheKey1();
 
-        rs = memberService.getCachedInt();
+        rs = cacheTestService.getCachedInt();
         System.out.println(rs);
     }
 
     @Test
     @DisplayName("캐시 수정")
     void t3() throws Exception {
-        int rs = memberService.getCachedInt();
+        int rs = cacheTestService.getCachedInt();
         System.out.println(rs);
 
-        rs = memberService.getCachedInt();
+        rs = cacheTestService.getCachedInt();
         System.out.println(rs);
 
-        memberService.putCacheKey1();
+        cacheTestService.putCacheKey1();
 
-        rs = memberService.getCachedInt();
+        rs = cacheTestService.getCachedInt();
+        System.out.println(rs);
+    }
+
+    @Test
+    @DisplayName("더하기 캐시")
+    void t4() throws Exception {
+        int rs = cacheTestService.plus(10, 20);
+        System.out.println(rs);
+
+        rs = cacheTestService.plus(5, 2);
+        System.out.println(rs);
+
+        rs = cacheTestService.plus(10, 20);
         System.out.println(rs);
     }
 }
