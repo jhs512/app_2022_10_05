e0c62c3a7bc9e030d3f445d993065dfaae9c720b
jhs512 <jangka512@gmail.com>
2022-10-13 13:27:47 +0900

    캐시 삭제

--- src/main/java/com/ll/exam/app__2022_10_05/app/member/service/MemberService.java
+++ src/main/java/com/ll/exam/app__2022_10_05/app/member/service/MemberService.java
@@ -4,6 +4,7 @@ import com.ll.exam.app__2022_10_05.app.member.entity.Member;
 import com.ll.exam.app__2022_10_05.app.member.repository.MemberRepository;
 import com.ll.exam.app__2022_10_05.app.security.jwt.JwtProvider;
 import lombok.RequiredArgsConstructor;
+import org.springframework.cache.annotation.CacheEvict;
 import org.springframework.cache.annotation.Cacheable;
 import org.springframework.stereotype.Service;
 import org.springframework.transaction.annotation.Transactional;
@@ -53,5 +54,9 @@ public class MemberService {
     public int getCachedInt() {
         System.out.println("getCachedInt 호출됨");
         return 5;
+    }
+
+    @CacheEvict("key1")
+    public void deleteCacheKey1() {
     }
 }
--- src/test/java/com/ll/exam/app__2022_10_05/CacheTests.java
+++ src/test/java/com/ll/exam/app__2022_10_05/CacheTests.java
@@ -22,13 +22,24 @@ class CacheTests {
     @DisplayName("캐시 사용")
     void t1() throws Exception {
         int rs = memberService.getCachedInt();
-
-        assertThat(rs).isGreaterThan(0);
         System.out.println(rs);
 
         rs = memberService.getCachedInt();
+        System.out.println(rs);
+    }
 
-        assertThat(rs).isGreaterThan(0);
+    @Test
+    @DisplayName("캐시 삭제")
+    void t2() throws Exception {
+        int rs = memberService.getCachedInt();
+        System.out.println(rs);
+
+        rs = memberService.getCachedInt();
+        System.out.println(rs);
+
+        memberService.deleteCacheKey1();
+
+        rs = memberService.getCachedInt();
         System.out.println(rs);
     }
 }
