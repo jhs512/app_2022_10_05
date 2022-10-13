3b68bf8481511afd570e8ac49f52d07e4916147a
jhs512 <jangka512@gmail.com>
2022-10-13 14:22:30 +0900

    캐시의 레퍼런스 키

--- src/test/java/com/ll/exam/app__2022_10_05/CacheTests.java
+++ src/test/java/com/ll/exam/app__2022_10_05/CacheTests.java
@@ -1,5 +1,6 @@
 package com.ll.exam.app__2022_10_05;
 
+import com.ll.exam.app__2022_10_05.app.cacheTest.dto.Person;
 import com.ll.exam.app__2022_10_05.app.cacheTest.service.CacheTestService;
 import org.junit.jupiter.api.DisplayName;
 import org.junit.jupiter.api.Test;
@@ -67,5 +68,20 @@ class CacheTests {
 
         rs = cacheTestService.plus(10, 20);
         System.out.println(rs);
+    }
+
+    @Test
+    @DisplayName("래퍼런스 매개변수")
+    void t5() throws Exception {
+        Person p1 = new Person(1, "홍길동1");
+        Person p2 = new Person(1, "홍길동2");
+
+        System.out.println(p1.equals(p2));
+
+        String personName = cacheTestService.getName(p1, 5);
+        System.out.println(personName);
+
+        personName = cacheTestService.getName(p2, 10);
+        System.out.println(personName);
     }
 }
